e06c4852a151550abf8da8bec90de6cdf0b15d3c
Doina Vasilev <99347603+doinav@users.noreply.github.com>
2024-02-26 23:50:57 +0100

    Update SSE.py

--- SSE.py
+++ SSE.py
@@ -1,4 +1,6 @@
-
+from gensim.models.phrases import Phrases, Phraser, ENGLISH_CONNECTOR_WORDS
+from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
 from gensim.models import Word2Vec
 import os
 from sklearn.metrics.pairwise import cosine_similarity
@@ -16,7 +18,7 @@ class SpreadIndexCalculator:
     def __init__(self, models):
         self.models = models
     
-    def spread_index_per_year(self, target_word):
+    def spread_index_per_year(self, target_word, topn):
         """
         Constructor for the SpreadIndexCalculator class.
 
@@ -25,7 +27,7 @@ class SpreadIndexCalculator:
         spread_indices = {}
         for year, model in self.models.items():
             if target_word in model.wv.key_to_index: 
-                similar_words = model.wv.most_similar(target_word, topn=15)
+                similar_words = model.wv.most_similar(target_word, topn=topn)
                 word_list = [word for word, _ in similar_words]
                 word_vectors = [model.wv[word] for word in word_list if word in model.wv.key_to_index]
                 
@@ -54,6 +56,47 @@ class SpreadIndexCalculator:
         spread_index = np.std([metrics.pairwise.euclidean_distances([vector], [mean_vector])[0][0] for vector in target_word_vectors])
         
         return spread_index
+    
+class LocalNeighborsMeasure:
+    def __init__(self, models):
+        self.models = models
+
+    def get_similar_neighbors(self, target_word, topn=30):
+        similar_words_per_year = {}
+        for year, model in self.models.items():
+            if target_word in model.wv.key_to_index:
+                similar_words = model.wv.most_similar(target_word, topn=topn)
+                similar_words_per_year[year] = [word for word, _ in similar_words]
+        return similar_words_per_year
+
+    def get_combined_similar_words(self, similar_words_per_year, year_t, year_t_plus_1):
+        combined_similar_words = set(similar_words_per_year.get(year_t, [])) | set(similar_words_per_year.get(year_t_plus_1, []))
+        return list(combined_similar_words)
+
+    def get_second_order_similarity(self, target_word, combined_similar_words, year_t, year_t_plus_1):
+        model_t = self.models[year_t]
+        model_t_plus_1 = self.models[year_t_plus_1]
+
+        s_t_vector = [cosine_similarity([model_t.wv[target_word]], [model_t.wv[neighbor]])[0][0] if neighbor in model_t.wv.key_to_index else 0 for neighbor in combined_similar_words]
+        s_t_plus_1_vector = [cosine_similarity([model_t_plus_1.wv[target_word]], [model_t_plus_1.wv[neighbor]])[0][0] if neighbor in model_t_plus_1.wv.key_to_index else 0 for neighbor in combined_similar_words]
+
+        return cosine_similarity([s_t_vector], [s_t_plus_1_vector])[0][0]
+
+    def calculate_consecutive_year_similarity(self, target_word, start_year, end_year_excluded):
+        year_similarities = {}
+
+        for year in range(start_year, end_year_excluded):
+            year_t = str(year)
+            year_t_plus_1 = str(year + 1)
+
+            if year_t in self.models and year_t_plus_1 in self.models:
+                similar_words_per_year = self.get_similar_neighbors(target_word)
+                combined_similar_words = self.get_combined_similar_words(similar_words_per_year, year_t, year_t_plus_1)
+                if combined_similar_words:
+                    second_order_similarity = self.get_second_order_similarity(target_word, combined_similar_words, year_t, year_t_plus_1)
+                    year_similarities[f"{year_t}-{year_t_plus_1}"] = second_order_similarity
+
+        return year_similarities
 
 def tfidf(tokens):
     frequency_per_year = {}
@@ -62,8 +105,7 @@ def tfidf(tokens):
         frequency_per_year[year] = {}
         document_len = len(document)
         for token in document:
-            token = token.lower()
-            frequency_per_year[year][token] = frequency_per_year[year].get(token, 0) + 1 / document_len # questa da capire
+            frequency_per_year[year][token] = frequency_per_year[year].get(token, 0) + 1 / document_len 
     
     # Compute doc frequency, as the frequency over the whole set
     doc_freq = {}
@@ -136,7 +178,6 @@ def find_most_similar_words_per_year(target_word, tfidf_per_year, yearly_embeddi
 
     for year, tfidf_dict in tqdm(tfidf_per_year.items()):
         model = yearly_embeddings[year]
-        # Check if the target word is in the model's vocabulary
         if target_word in model.key_to_index:
             target_embedding = model[target_word]
 
@@ -148,17 +189,44 @@ def find_most_similar_words_per_year(target_word, tfidf_per_year, yearly_embeddi
                     and (not words_to_exclude or word not in words_to_exclude)):
                     word_embeddings[word] = model[word]
 
-            # Select words based on TF-IDF > 0 and that have embeddings
+            # Select words based on TF-IDF > 0 
             filtered_words = {word: tfidf for word, tfidf in tfidf_dict.items() if word in word_embeddings and tfidf > 0}
 
-            # Calculate similarities between target word and other words
             if word_embeddings:
                 similarities = {}
                 for word, embedding in word_embeddings.items():
                     similarities[word] = cosine_similarity([target_embedding], [embedding])[0][0]
 
-                # Sort words by similarity and take the top n
                 top_similar_words = sorted(similarities, key=similarities.get, reverse=True)[:top_n]
                 similar_words_per_year[year] = [(word, similarities[word]) for word in top_similar_words]
-
     return similar_words_per_year
+
+def unite_concepts(tokens, nsc_concepts):
+    for year, lists in tokens.items():
+        for lista in lists:
+            i = 0
+            while i < len(lista) - 1:
+                for el in nsc_concepts:
+                    if len(el) == 2 and lista[i] == el[0] and lista[i + 1] == el[1]: 
+                        lista[i] = f'{el[0]}_{el[1]}'
+                        del lista[i + 1]
+                        break  
+                else: 
+                    i += 1  
+
+    return tokens
+
+def build_bigram_dictionary(documents, min_count, threshold):
+    phrases = Phrases(documents, min_count=min_count, threshold=threshold, connector_words=ENGLISH_CONNECTOR_WORDS)
+    bigram = Phraser(phrases)
+    tokens_text_with_bigrams = [bigram[doc] for doc in documents]
+    return tokens_text_with_bigrams
+
+def calculate_jaccard_similarity(set1, set2):
+    intersection = set1.intersection(set2)
+    union = set1.union(set2)
+    if not union:
+        return 0
+    jaccard_similarity = len(intersection) / len(union)
+    
+    return jaccard_similarity
